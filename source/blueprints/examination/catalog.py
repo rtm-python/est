f1244d62958d3789cec02f71e3c93f1c36d3cab4
rtm-python <rtm.python@gmail.com>
2020-11-11 00:25:57 +0600

    Create filter, info, edit examination pages

--- source/blueprints/examination/catalog.py
+++ source/blueprints/examination/catalog.py
@@ -15,6 +15,13 @@ from blueprints.__locale__ import __
 
 # Additional libraries import
 from flask import render_template
+from flask import redirect
+from flask import request
+from flask import url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import BooleanField
+from wtforms import SubmitField
 
 # Test data
 test_examinations = [
@@ -62,12 +69,59 @@ test_examinations = [
 #test_examinations += test_examinations
 
 
-@blueprint.route('/', methods=('GET',))
-@blueprint.route('/catalog/', methods=('GET',))
+class FilterForm(FlaskForm):
+	"""
+	This is FilterForm class to retrieve form data.
+	"""
+	filter_name = StringField()
+	filter_plugin = StringField()
+	filter_hide_global = BooleanField()
+	submit = SubmitField()
+
+	def set_fields_from_request(self) -> None:
+		"""
+		Set form fields to values from request.
+		"""
+		self.filter_name.data = request.args.get('filter_name')
+		self.filter_plugin.data = request.args.get('filter_plugin')
+		self.filter_hide_global.data = request.args.get('filter_hide_global')
+
+
+class EditorForm(FlaskForm):
+	"""
+	This is EditorForm class to retrieve form data.
+	"""
+	editor_name = StringField()
+	editor_description = StringField()
+	submit = SubmitField()
+
+	def __init__(self, item: dict) -> 'EditorForm':
+		"""
+		"""
+		pass
+
+	def set_fields_from_request(self) -> None:
+		"""
+		Set form fields to values from request.
+		"""
+		pass
+
+
+@blueprint.route('/', methods=('GET', 'POST'))
+@blueprint.route('/catalog/', methods=('GET', 'POST'))
 def get_examination_catalog():
 	"""
 	Return examination catalog page.
 	"""
+	filter = FilterForm()
+	if filter.validate_on_submit():
+		return redirect(url_for(
+			'examination.get_examination_catalog',
+			filter_name=filter.filter_name.data,
+			filter_plugin=filter.filter_plugin.data,
+			filter_hide_global=filter.filter_hide_global.data
+		))
+	filter.set_fields_from_request()
 	pagination = get_pagination(len(test_examinations))
 	pagination['endpoint'] = 'examination.get_examination_catalog'
 	examinations = test_examinations[
@@ -81,26 +135,47 @@ def get_examination_catalog():
 		)
 	return render_template(
 		'examination/catalog.html',
+		filter=filter,
 		examinations=examinations,
 		caption=caption,
 		pagination=pagination
 	)
 
 
-@blueprint.route('/catalog/view/<uid>/', methods=('GET',))
+@blueprint.route('/view/<uid>/', methods=('GET',))
 def get_examination(uid: str):
 	"""
 	Return examination view page.
 	"""
-	return 'Examination View Page'
+	filter = FilterForm()
+	filter.set_fields_from_request()
+	pagination = get_pagination(len(test_examinations))
+	item = test_examinations[0]
+	return render_template(
+		'examination/info.html',
+		filter=filter,
+		pagination=pagination,
+		item=item
+	)
 
 
-@blueprint.route('/catalog/edit/<uid>/', methods=('GET',))
+@blueprint.route('/edit/<uid>/', methods=('GET', 'POST'))
 def edit_examination(uid: str):
 	"""
 	Return examination edit page.
 	"""
-	return 'Examination Edit Page'
+	filter = FilterForm()
+	filter.set_fields_from_request()
+	pagination = get_pagination(len(test_examinations))
+	item = test_examinations[0]
+	editor = EditorForm(item)
+	return render_template(
+		'examination/edit.html',
+		filter=filter,
+		pagination=pagination,
+		editor=editor,
+		uid=uid
+	)
 
 
 @blueprint.route('/catalog/delete/<uid>/', methods=('GET',))
